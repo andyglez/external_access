3ddab557551cb50e4371e39a426d6915b69cd9da
Andy Gonzalez <aglez9538@gmail.com>
2019-03-13 12:22:37 -0400

    Creating new sign up template

--- app.py
+++ app.py
@@ -4,19 +4,25 @@ from settings import database as db
 
 app = Flask(__name__)
 app.secret_key = str(urandom(24))
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+
+db.query(db.create_db_roles, False)
+db.query(db.create_pending, False)
+if len(db.query('select * from DBRoles')) == 0:
+    db.query(db.insert_roles)
+if __name__ == '__main__':    
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def start():
     if request.method == 'GET':
         return render_template('login.html')
-    data = query('select UserName, Password from Users where Username=\'' + request.form['user']+'\' and Password=\'' + request.form['password']+ '\'')
+    data = db.query('select UserName, Password from Users where Username=\'' + request.form['user']+'\' and Password=\'' + request.form['password']+ '\'')
     if len(data) == 0:
         flash('Incorrect User Name or Password')
         return redirect(url_for('start'))
     usr, pwd = data[0]
-    result, _ = query('select roles, username from DBRoles where UserName = \'' + usr + '\'')[0]
+    result, _ = db.query('select roles, username from DBRoles where UserName = \'' + usr + '\'')[0]
     roles = result.split(',')
     return index(usr, roles)
 
@@ -26,12 +32,7 @@ def index(username, roles):
 
 @app.route('/request', methods=['GET', 'POST'])
 def request_form():
+    if request.method == 'GET':
+        return render_template('request.html')
     return ''
 
-def query(s):
-    cursor = db.get_connection.cursor()
-    cursor.execute(s)
-    lst = [i for i in cursor.fetchall()]
-    cursor.close()
-    db.get_connection.commit()
-    return lst
--- settings/database.py
+++ settings/database.py
@@ -12,4 +12,36 @@ dbse = 'radius'
 kargs = dict(use_unicode=1, connect_timeout=10)
 
 get_connection = MySQLdb.Connection(*(host, user, pwrd, dbse), **kargs)
+
+def query(s, is_select=True):
+    cursor = get_connection.cursor()
+    cursor.execute(s)
+    lst = [i for i in cursor.fetchall()] if is_select else []
+    cursor.close()
+    get_connection.commit()
+    return lst
 #---------------------------------------------------------------------
+
+#--------------------------- DB Requirements -------------------------
+# Creation of tables and data required for administration purposes
+
+# Table creation
+create_db_roles = '''create table if not exists DBRoles(
+                        entry_id int auto_increment,
+                        username varchar(255) not null,
+                        roles varchar(255) not null,
+                        primary key(entry_id)
+                        ) Engine=innodb'''
+
+create_pending = '''create table if not exists Pending(
+                        username varchar(255) not null,
+                        name varchar(255) not null,
+                        password varchar(255),
+                        phone varchar(100) not null,
+                        primary key(username)
+                        ) Engine=innodb'''
+
+# Data insertion
+insert_roles = '''insert into DBRoles (username, roles)
+                select UserName, \'default\' 
+                from Users'''
